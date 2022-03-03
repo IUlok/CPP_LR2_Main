dfb3c2e2c543206ba6d0ff78bdcd572615d45f50
Ulok <elfbrus@gmail.com>
2022-03-03 22:33:23 +0300

    hyh

--- code.cpp
+++ code.cpp
@@ -8,23 +8,24 @@ using namespace std;
 int main() {
   char tmp[80];
   int len;
-  char *str;
+  char* str;
 
   setlocale(LC_ALL, "Russian");
 
   do {
+    cout << "Ввод: ";
     cin.getline(tmp, 80);
     len = strlen(tmp)+1;
     str = new char[len];
-    strncpy(str, len, tmp);
+    strncpy(str, tmp, len);
 
     while (*str) {
       *str=(*str)++;
       str++;
     }
-    cout << str << endl;
+    cout << (*str) << endl;
     delete []str;
   }
-  while(tmp[0]&&!isalpha(tmp[len-1])&&!isalpha(tmp[len-2]));
+  while(isalpha(tmp[len-1])&&isalpha(tmp[len-2]));
   return 0;
 }
