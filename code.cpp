563d195dadb80790a8db21c30b7fbc87a0409b28
Ulok <elfbrus@gmail.com>
2022-03-04 01:32:52 +0300

    AMOGUS

--- code.cpp
+++ code.cpp
@@ -1,3 +1,5 @@
+// В 22 строке можно написать проверку. Но нужно-ли? Хммммммммм
+//ѡ҄ѾҒѳ!.!Ѽ҄҈҉ѹѺ!��ҁѶ��ѿѵѱѳѱ҃ѶѼҍ "c
 #include <iostream>
 #include <cctype>
 #include <cstring>
@@ -10,7 +12,7 @@ int main() {
   int len;
   char* str;
 
-  // setlocale(LC_ALL, "Russian");
+  setlocale(LC_ALL, "Russian");
 
   do {
     int n=0;
@@ -21,11 +23,19 @@ int main() {
     strncpy(str, tmp, len); // Использована эта ф-ция вместо strcpy_s, т.к. эту ф-цию не обрабатывает компилятор g++. Она не такая безопасная, но в текущей программе проблем не возникнет
 
     for (int i = 0; i<len;i++) {
+      if (*(str+i)=='z') {
+      *(str+i)='a';
+      continue;
+      }
+      else if (*(str+i)=='Z') {
+        *(str+i)='A';
+        continue;
+      }
       *(str+i)=*(str+i)+1;
     }
 
     cout << str << endl;
     delete []str;
-  } while(tmp[0] && (isalpha(tmp[len-3]) || isalpha(tmp[len-2])));
+  } while(tmp[0] && (isalpha(tmp[len-3]) && isalpha(tmp[len-2])));
   return 0;
 }
