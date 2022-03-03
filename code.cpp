fd31128c8f12e31f70af1f66a8718861198b21ad
Ulok <elfbrus@gmail.com>
2022-03-04 01:04:28 +0300

    GOD DAMN

--- code.cpp
+++ code.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <cctype>
-#include <clocale>
 #include <cstring>
 #include <string.h>
+#include <clocale>
 using namespace std;
 
 int main() {
@@ -10,21 +10,22 @@ int main() {
   int len;
   char* str;
 
-//   setlocale(LC_ALL, "Russian");
+  // setlocale(LC_ALL, "Russian");
 
   do {
+    int n=0;
     cout << "Ввод: ";
     cin.getline(tmp, 80);
     len = strlen(tmp)+1;
     str = new char[len];
-    strncpy(str, tmp, len);
+    strncpy(str, tmp, len); // Использована эта ф-ция вместо strcpy_s, т.к. эту ф-цию не обрабатывает компилятор g++. Она не такая безопасная, но в текущей программе проблем не возникнет
 
-    // while (*str) {
-    //   *str=(*str)++;
-    //   *str++;
-    // }
+    for (int i = 0; i<len;i++) {
+      *(str+i)=*(str+i)+1;
+    }
+
     cout << str << endl;
     delete []str;
-  } while(tmp[0] && isalpha(tmp[len-1]) && isalpha(tmp[len-2]));
+  } while(tmp[0] && (isalpha(tmp[len-3]) || isalpha(tmp[len-2])));
   return 0;
 }
